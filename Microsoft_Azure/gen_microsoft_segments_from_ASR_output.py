a8ba12730b2ef231d145587737121289dba2e3d0
Haley Northrup <hnorthru@umich.edu>
2020-12-08 18:34:55 -0500

    add word timing

--- Microsoft_Azure/gen_microsoft_segments_from_ASR_output.py
+++ Microsoft_Azure/gen_microsoft_segments_from_ASR_output.py
@@ -2,12 +2,18 @@ import os
 import argparse
 import pandas as pd
 import numpy as np
+import pickle 
 from pydub import AudioSegment
 
 from IPython import embed 
 
 """
 Script for generating segments from Microsoft Azure ASR output 
+
+segment metdata csv file  
+/segments 
+/word_timing 
+
 """
 
 class GenMicrosoftSegments:
@@ -44,14 +50,21 @@ class GenMicrosoftSegments:
                 sorted_entries.loc[0] = combined_df.loc[idx, :]
             else:
                 sorted_entries = combined_df.loc[idx].sort_values(sort_column)
-            segment_dict[idx] = sorted_entries.drop(['word_timing', 'confidence'], axis=1)  
+            segment_dict[idx] = sorted_entries #.drop(['word_timing', 'confidence'], axis=1)  
         return segment_dict
 
     def generate_segments(self, output_dir, wav_dir):
         """
         Generate and save segment .wav files and metadata based on segment timing (segment_dict)
-        :return: none
+
+        :param output_dir: Path to output directory
+        :param wav_dir: Path to directory with .wav files used to generate segments  
         """
+        #make output directory 
+        seg_outdir = os.path.join(output_dir, 'segments') 
+        if not os.path.exists(seg_outdir): 
+            os.makedirs(seg_outdir) 
+
         seg_meta_df = pd.DataFrame()
         ct = 0
         #read segment dict #iterate over calls
@@ -73,29 +86,86 @@ class GenMicrosoftSegments:
                 orig_audio = AudioSegment.from_wav(wave_file)
                 segment_audio = orig_audio[int(np.floor(start_ms)):int(np.ceil(end_ms))]
                                 
-                #TODO is the segment_id name "ms_" confusing with milliseconds ("_ms") 
-                # save segment to .wav file ("ms_<call_id>_<segment_number>")
-                seg_id = 'ms_' + str(call_id) + '_' + str(s) 
-                #orig_audio.export(os.path.join(output_dir, str(call_id) + '.wav'))
-                segment_audio.export(os.path.join(output_dir, seg_id + '.wav'), format="wav")
+                #Segment_id "ma<int>" (ma = Microsoft Azure) 
+                seg_id = 'ma' + str(ct) 
+                segment_audio.export(os.path.join(seg_outdir, seg_id + '.wav'), format="wav")
 
                 #Save segment metadata 
+                seg_df.loc[sidx, 'segment_id'] = seg_id 
                 seg_meta_df.loc[ct, 'segment_id'] =  seg_id
                 seg_meta_df.loc[ct, 'call_id'] = int(call_id)
                 seg_meta_df.loc[ct, 'segment_number'] = int(s) #John/Soheil called "segment_order" 
                 seg_meta_df.loc[ct, 'subject_id'] = self.call_meta_df.loc[cidx, 'subject_id'].astype(int).values[0]
                 seg_meta_df.loc[ct, 'call_datetime'] = self.call_meta_df.loc[cidx, 'date_time'].values[0]
                 seg_meta_df.loc[ct, 'is_assessment'] = self.call_meta_df.loc[cidx, 'is_assessment'].values[0]
+                seg_meta_df.loc[ct, 'device'] = self.call_meta_df.loc[cidx, 'device'].values[0]
                 seg_meta_df.loc[ct, 'duration_ms'] = int(duration_ms) #John/Soheil call "segment_length"
                 seg_meta_df.loc[ct, 'start_ms'] = int(start_ms) 
                 seg_meta_df.loc[ct, 'end_ms'] = int(end_ms) 
-                seg_meta_df.loc[ct, 'ms_transcription'] = seg_df.loc[sidx, 'text'].values[0] 
-                ct = ct + 1 
+                seg_meta_df.loc[ct, 'confidence'] = seg_df.loc[sidx, 'confidence'].values[0]
+                seg_meta_df.loc[ct, 'ma_transcription'] = seg_df.loc[sidx, 'text'].values[0] 
+                ct = ct + 1  
+            #update segment_dict with segment_ids 
+            self.segment_dict[call_id] = seg_df     
         #Save transcript version 
-        seg_meta_df.to_csv(os.path.join(output_dur, 'seg_meta_test_with_ASR_trans.csv'), index=False) 
+        seg_meta_df.to_csv(os.path.join(output_dir, 'priori_v1_ma_segments_with_trans.csv'), index=False) 
         #Save no transcript version 
-        seg_meta_df = seg_meta_df.drop(['ms_transcription'], axis=1)
-        seg_meta_df.to_csv(os.path.join(output_dir, 'seg_meta_test.csv'), index=False) 
+        seg_meta_df = seg_meta_df.drop(['ma_transcription'], axis=1)
+        seg_meta_df.to_csv(os.path.join(output_dir, 'priori_v1_ma_segments.csv'), index=False) 
+
+
+    def gen_segment_word_timing_files(self, output_dir):
+        """
+        Save segment word timing to pickle files (list(dict(word, duration (ms), offset (ms))))
+
+        :param output_dir: Path to output directory 
+        """
+        #make output directory 
+        wt_outdir = os.path.join(output_dir, 'word_timing') 
+        if not os.path.exists(wt_outdir): 
+            os.makedirs(wt_outdir)
+
+        #read segment dict #iterate over calls
+        for call_id, seg_df in self.segment_dict.items():
+            for s in range(0, seg_df.shape[0]):#iterate over segments
+                seg = seg_df.iloc[s]
+                seg = self._parse_word_timing_str(seg)
+                #update units (100 ns) to milliseconds 
+                for w in range(0, len(seg['word_timing'])):
+                    seg['word_timing'][w]['Duration'] = seg['word_timing'][w]['Duration']* 10**-4 
+                    seg['word_timing'][w]['Offset'] = seg['word_timing'][w]['Offset']* 10**-4                
+                #save to pickle file 
+                fn = os.path.join(wt_outdir, seg['segment_id'] + '.pkl')
+                with open(fn, 'wb') as file:
+                    pickle.dump(seg['word_timing'], file, protocol=pickle.HIGHEST_PROTOCOL) 
+
+    def _parse_word_timing_str(self, seg_df):
+        """
+        Parse word_timing string(list(Dict)) and generate List(Dict) with a dictionary for each word (duration, offset, word)
+        :param seg_df: single segment dataframe with word-level timing information. Each row (duration, offset, str(list(Dict)). 
+        :return: seg_df: single segment dataframe with each row (duration, offset, list(Dict). 
+        """
+        word_timing_list = []
+        #separate each string word dictionary 
+        word_timing_str = seg_df['word_timing'].strip('][').split("},")
+        
+        #get information for each word dictionary 
+        for ws in word_timing_str:
+            word_dict = {}
+            ws = ws.strip(" ")
+            dl = ws.strip("}{").split(",")
+            for d in dl:
+                d2 = d.split(":")
+                k = d2[0].strip(" ").strip("''")
+                v = d2[1].strip(" ")
+                if v.isnumeric(): #Duration, Offset are integers
+                    v = int(v) 
+                else: #Word is a string
+                    v = v.strip("''")
+                word_dict[k] = v
+            word_timing_list.append(word_dict) 
+        seg_df['word_timing'] = word_timing_list   
+        return seg_df 
 
 
 def _read_file_by_lines(filename):
@@ -122,7 +192,7 @@ def main():
     recognition_result_files = _read_file_by_lines(args.ms_asr_output_files)
     seg_extractor = GenMicrosoftSegments(recognition_result_files, args)
     seg_extractor.generate_segments(args.output_dir, args.wav_dir)
-
+    seg_extractor.gen_segment_word_timing_files(args.output_dir) 
 
 if __name__ == '__main__':
     main()
