c03f2eddd1681f6db7e08d27e1a0b1a437a36727
Haley Northrup <hnorthru@umich.edu>
2020-12-27 08:51:54 -0500

    check combined

--- Microsoft_Azure/check_segmentation_output.py
+++ Microsoft_Azure/check_segmentation_output.py
@@ -16,12 +16,14 @@ def _read_file_by_lines(filename):
 
 
 #PATHS TO SEGMENT OUTPUTS 
-num_jobs = 10 
+num_jobs = 10
+#transcript_paths = '/nfs/turbo/McInnisLab/hnorthru/code/audio_segmentation/Microsoft_Azure/'
 transcript_paths = '/nfs/turbo/McInnisLab/hnorthru/code/audio_segmentation/Microsoft_Azure/tran_paths'
+#seg_set_paths = [os.path.join(transcript_paths, 'ma_pv1_set_paths.txt')]
 seg_set_paths = [os.path.join(transcript_paths, 'ma_pv1_set_paths_' + str(i) + '.txt') for i in range(0, num_jobs)]
 output_dir='/nfs/turbo/McInnisLab/PRIORI_v1_Microsoft_Azure/PRIORI-v1-Microsoft-segments'
+#seg_folders = [output_dir]
 seg_folders = [os.path.join(output_dir, 'seg_' + str(i))  for i in range(0, num_jobs)]
-
 
 #CHECK COUNTS 
 for n in range(0, num_jobs):
@@ -33,7 +35,6 @@ for n in range(0, num_jobs):
     wavs = os.path.join(sf, 'segments')
     word_time = os.path.join(sf, 'word_timing')
     meta_df = pd.read_csv(os.path.join(sf, 'priori_v1_ma_segments.csv'))
-    meta_df.shape[0]
     if len(os.listdir(wavs)) != meta_df.shape[0]:
         print('*****Inconsistent # segment wav files as metadata rows')
     elif len(os.listdir(word_time)) != meta_df.shape[0]:
@@ -75,4 +76,39 @@ for n in range(0, num_jobs):
         print(word_timing_example) 
 
 
+# Total Number of Files Matches sum of individual jobs 
+all_seg_meta_df = pd.read_csv(os.path.join(output_dir, 'priori_v1_ma_segments.csv'))
+file_counts = []
+for n in range(0, num_jobs):
+    sf = seg_folders[n]
+    #print('Folder: ' + sf)
+    meta_df = pd.read_csv(os.path.join(sf, 'priori_v1_ma_segments.csv'))
+    #print(meta_df.shape[0])
+    file_counts.append(meta_df.shape[0]) 
 
+if all_seg_meta_df.shape[0] != np.sum(file_counts):
+    print('TOTAL FILE COUNTS DO NOT MATCH!!!!')
+else: 
+    print('Total File Counts Match!')
+    print(all_seg_meta_df.shape)
+
+#CHECK START AND END FILES 
+cols = ['call_id', 'segment_num', 'subject_id', 'call_datetime', 'is_assessment', 'device', 'duration_ms']
+start_idx = 0 
+for n in range(0, num_jobs):
+    sf = seg_folders[n]
+    print('Folder: ' + sf)
+    meta_df = pd.read_csv(os.path.join(sf, 'priori_v1_ma_segments.csv'))
+    end_idx = start_idx + meta_df.shape[0] - 1
+
+    #Check start file 
+    print(start_idx)
+    if meta_df.iloc[0][cols].values.any() != all_seg_meta_df.iloc[start_idx][cols].values.any():
+        print('Start files not aligned') 
+
+    #Check end file  
+    print(end_idx) 
+    if meta_df.iloc[meta_df.shape[0]-1][cols].values.any() != all_seg_meta_df.iloc[end_idx][cols].values.any():
+        print('End files not aligned') 
+        
+    start_idx = end_idx + 1
